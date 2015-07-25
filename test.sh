9f0480a1d6801f5127adab74662cab50442cea1a
Laurent Cetinsoy <laurent.cetinsoy@gmail.com>
2015-07-25 11:49:26 +0100

    removed volume from testing container

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 docker run -p 127.0.0.1:5432:5432 --name pgsqld -e POSTGRES_PASSWORD=test -d postgres
 docker build -t image-test ./
-docker run -ti --name testing -v $(pwd):/var/www --link pgsqld:pgsqld image-test php /var/www/vendor/bin/atoum -d /var/www/tests
+docker run -ti --name testing --link pgsqld:pgsqld image-test php /var/www/vendor/bin/atoum -d /var/www/tests
 docker rm -v -f testing
 docker rm -v -f pgsqld
