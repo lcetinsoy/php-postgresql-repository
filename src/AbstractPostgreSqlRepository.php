2bffd5a0c1e982a2bb1e36d13b0be35291484911
Laurent Cetinsoy <laurent.cetinsoy@gmail.com>
2015-09-02 17:47:24 +0100

    changed serialize method to abstract and uppercased sql keywords

--- src/AbstractPostgreSqlRepository.php
+++ src/AbstractPostgreSqlRepository.php
@@ -28,28 +28,26 @@ abstract class AbstractPostgreSqlRepository {
 
     public function getAggregateById($id) {
 
-        $prep = $this->connection->prepare('select data FROM ' . $this->getTableName() . ' WHERE id=:id');
+        $prep = $this->connection->prepare('SELECT data FROM ' . $this->getTableName() . ' WHERE id=:id');
         $prep->bindParam(':id', $id);
         $prep->execute();
         return $prep->fetchColumn();
     }
 
     public function findBy($criteria, $value) {
-        $query = 'select * FROM ' . $this->getTableName() . ' WHERE data ->> \'' . $criteria . '\'=:' . $criteria;
+        $query = 'SELECT * FROM ' . $this->getTableName() . ' WHERE data ->> \'' . $criteria . '\'=:' . $criteria;
         $prep = $this->connection->prepare($query);
         $prep->bindParam(':' . $criteria, $value);
         $prep->execute();
         return $prep->fetchAll();
     }
 
-    protected function serialize($aggregate) {
-        return $this->serializer->serialize($aggregate);
-    }
+    abstract protected function serialize($aggregate);
 
     protected function insertAsJson($json) {
 
         $this->connection->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
-        $prep = $this->connection->prepare('insert into ' . $this->getTableName() . ' (data) VALUES (:json) RETURNING id');
+        $prep = $this->connection->prepare('INSERT INTO ' . $this->getTableName() . ' (data) VALUES (:json) RETURNING id');
         $prep->bindParam(':json', $json);
         $prep->execute();
         return $prep->fetchColumn();
@@ -57,7 +55,7 @@ abstract class AbstractPostgreSqlRepository {
 
     protected function updateAsJson($id, $json) {
 
-        $prep = $this->connection->prepare('update ' . $this->getTableName() . ' SET data =:json WHERE id=:id');
+        $prep = $this->connection->prepare('UPDATE ' . $this->getTableName() . ' SET data =:json WHERE id=:id');
 
         $prep->bindParam(':id', $id);
         $prep->bindParam(':json', $json);
